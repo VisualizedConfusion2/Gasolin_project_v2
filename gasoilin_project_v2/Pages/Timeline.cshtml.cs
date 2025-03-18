165a3208f944a97a795cde33ab2d5f1fc0ec00c6
VisualizedConfusion2 <thh005.edu.zealand.dk>
2025-03-18 12:24:48 +0100

    commit index changes

---
+++ gasoilin_project_v2/Pages/Timeline.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace gasoilin_project_v2.Pages
+{
+    public class HistoryModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
